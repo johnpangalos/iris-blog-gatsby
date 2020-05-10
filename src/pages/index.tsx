eeb4fe576ebca438930fd860f4115b63972f67d4
John Pangalos <jpangs88@gmail.com>
2020-05-10 09:50:23 +0200

    Get things working

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,15 +13,57 @@ type Data = {
       title: string
     }
   }
+  allMarkdownRemark: {
+    edges: {
+      node: {
+        excerpt: string
+        frontmatter: {
+          title: string
+          date: string
+          description: string
+        }
+        fields: {
+          slug: string
+        }
+      }
+    }[]
+  }
 }
 
 const BlogIndex = ({ data, location }: PageProps<Data>) => {
   const siteTitle = data.site.siteMetadata.title
+  const posts = data.allMarkdownRemark.edges
 
   return (
     <Layout location={location} title={siteTitle}>
       <SEO title="All posts" />
       <Bio />
+      {posts.map(({ node }) => {
+        const title = node.frontmatter.title || node.fields.slug
+        return (
+          <article key={node.fields.slug}>
+            <header>
+              <h3
+                style={{
+                  marginBottom: rhythm(1 / 4),
+                }}
+              >
+                <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
+                  {title}
+                </Link>
+              </h3>
+              <small>{node.frontmatter.date}</small>
+            </header>
+            <section>
+              <p
+                dangerouslySetInnerHTML={{
+                  __html: node.frontmatter.description || node.excerpt,
+                }}
+              />
+            </section>
+          </article>
+        )
+      })}
     </Layout>
   )
 }
@@ -33,6 +75,21 @@ export const pageQuery = graphql`
     site {
       siteMetadata {
         title
+      }
+    }
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      edges {
+        node {
+          excerpt
+          fields {
+            slug
+          }
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            title
+            description
+          }
+        }
       }
     }
   }
