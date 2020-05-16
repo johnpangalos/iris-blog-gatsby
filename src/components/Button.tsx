252260ab360606eb23ea233a4c9f9cb96ef41742
John Pangalos <jpangs88@gmail.com>
2020-05-16 12:52:07 +0200

    Fix for colors being purged

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -8,8 +8,6 @@ type ButtonProps = {
   contained?: boolean
 }
 
-const color = "orange"
-
 export const Button = ({
   children,
   onClick,
@@ -21,9 +19,9 @@ export const Button = ({
     className={cn(
       "text-lg uppercase px-4 py-2 rounded duration-300 ease-in-out",
       {
-        [`bg-${color}-500 shadow hover:shadow-lg transition-shadow text-white`]: contained,
-        [`border border-${color}-500`]: outlined,
-        [`text-${color}-500 hover:bg-${color}-200 transition-colors`]: !contained,
+        [`bg-orange-500 shadow hover:shadow-lg transition-shadow text-white`]: contained,
+        [`border border-orange-500`]: outlined,
+        [`text-orange-500 hover:bg-orange-200 transition-colors`]: !contained,
       }
     )}
     onClick={onClick}
